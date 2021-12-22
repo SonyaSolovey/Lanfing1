3d811260f0a954fc87c09440e14b4860731d51f7
Sonya Soloviova <94216492+SonyaSolovey@users.noreply.github.com>
2021-12-22 19:52:51 +0400

    Initial commit

---
+++ js.js
@@ -0,0 +1,38 @@
+function changeALL() {
+    document.getElementById('blok22change').style.display = "none";
+    document.getElementById('blok222').style.display = "block";
+    document.getElementById('all').style.backgroundColor = "#fffaff";
+    document.getElementById('web').style.backgroundColor = "#ad935d";
+    document.getElementById('apps').style.backgroundColor = "#ad935d";
+    document.getElementById('icon').style.backgroundColor = "#ad935d";
+}
+
+function changeWEB() {
+    document.getElementById('blok222').style.display = "none";
+    document.getElementById('blok22change').style.display = "block";
+    document.getElementById('changeme1').src="cod/11111.png";
+    document.getElementById('web').style.backgroundColor = "#fffaff";
+    document.getElementById('all').style.backgroundColor = "#ad935d";
+    document.getElementById('apps').style.backgroundColor = "#ad935d";
+    document.getElementById('icon').style.backgroundColor = "#ad935d";
+}
+
+function changeAPPS() {
+    document.getElementById('blok222').style.display = "none";
+    document.getElementById('blok22change').style.display = "block";
+    document.getElementById('changeme1').src="cod/22222.png";
+    document.getElementById('apps').style.backgroundColor = "#fffaff";
+    document.getElementById('all').style.backgroundColor = "#ad935d";
+    document.getElementById('web').style.backgroundColor = "#ad935d";
+    document.getElementById('icon').style.backgroundColor = "#ad935d";
+}
+
+function changeICON() {
+    document.getElementById('blok222').style.display = "none";
+    document.getElementById('blok22change').style.display = "block";
+    document.getElementById('changeme1').src="cod/33333.png";
+    document.getElementById('icon').style.backgroundColor = "#fffaff";
+    document.getElementById('all').style.backgroundColor = "#ad935d";
+    document.getElementById('web').style.backgroundColor = "#ad935d";
+    document.getElementById('apps').style.backgroundColor = "#ad935d";
+}
